7d75749c2ef8e1a1664b031ef87f498ef2c83b54
Kaled-Elawady <82104020+Kaled-Elawady@users.noreply.github.com>
2022-11-07 18:52:44 +0200

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -8,14 +8,25 @@ def d_operator() :
   elif operator == "/" :
     print (sum1/sum2)
 
-operation = input("Enter your operation : ")
-n_loop = -1
-for i in operation :
-  n_loop +=1
-  try :
-    i = int(i)
-  except ValueError :
-    sum1 = int(operation [:n_loop])
-    sum2 = int(operation [n_loop + 1 :])
-    operator = i
-d_operator() 
+
+while True :
+  print()
+  operation = input("Enter your operation : ")
+  print()
+  n_loop = -1
+  for i in operation :
+    n_loop +=1
+    try :
+      i = int(i)
+    except ValueError :
+      sum1 = int(operation [:n_loop])
+      sum2 = int(operation [n_loop + 1 :])
+      operator = i
+
+  d_operator()
+  print()
+  u_input = input("c to calculate another operation or any different key te exit : ") 
+  if u_input == "c" :
+    continue
+  else :
+    break
